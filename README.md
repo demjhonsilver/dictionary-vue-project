d9ebfb7e7068520cdac5e2e0db4bd35c4d61f58b
demjhonsilver <djsilverph@gmail.com>
2023-10-11 13:54:24 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<p align="center">
+
+<img src="https://raw.githubusercontent.com/demjhonsilver/dictionary-vue-project/main/img/screenshot.png" alt="Logo" width="70" height="70"/>
+
+</p>
+
+<h1>Dictionary Vue Application</h1>
+
+- [Description](#description)
+- [Author](#author)
+
+
+
+## Description
+
+This is asimple dictionary web application using Vue 3.
+
+
+## Author
+
+
+Demjhon Silver
+
+
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="app-container">
+    <header>
+      <h1>Dictionary - Vue Application</h1>
+    </header>
+
+    <main>
+      <div class="search-container">
+        <input v-model="searchTerm" @input="searchMeaning" placeholder="Enter a word" class="search-input">
+        <button class="search-button" @click="searchMeaning">Search</button>
+      </div>
+      <div v-if="meaning" class="results">
+        <h2 class="word">{{ meaning.word }}</h2>
+        <p class="phonetic">Phonetic: {{ meaning.phonetic }}</p>
+        <ul v-if="meaning.meanings.length">
+          <li v-for="meaningItem in meaning.meanings" :key="meaningItem.partOfSpeech" class="part-of-speech">
+            <ul class="meaning-list">
+              <li v-for="definition in meaningItem.definitions" :key="definition.definition" class="definition">
+                {{ definition.definition }}
+                <p v-if="definition.example" class="example">Example: {{ definition.example }}</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const searchTerm = ref('');
+    const meaning = ref(null);
+
+    const searchMeaning = async () => {
+      if (searchTerm.value) {
+        try {
+          const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm.value}`);
+          if (response.status === 200) {
+            const data = await response.json();
+            meaning.value = data[0];
+          } else {
+            meaning.value = null;
+          }
+        } catch (error) {
+          meaning.value = null;
+          console.error("Error fetching data: " + error);
+        }
+      }
+    };
+
+    return { searchTerm, meaning, searchMeaning };
+  },
+};
+</script>
